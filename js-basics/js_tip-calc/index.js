d6fbe9a9de6df283ad433de03876d65a954f73f6
Eduard Ekkel <eduardekkel.98@gmail.com>
2024-08-01 12:03:28 +0200

    finished challenge

---
+++ js-basics/js_tip-calc/index.js
@@ -0,0 +1,10 @@
+let mealCost = 25;
+let tipPercentage = 32;
+let tipAmount = mealCost * (tipPercentage / 100);
+let totalCost = mealCost + tipAmount;
+
+console.log(mealCost);
+console.log(tipPercentage);
+console.log(tipAmount);
+console.log(totalCost);
+console.log("The total cost of your bill is: €" + totalCost);
