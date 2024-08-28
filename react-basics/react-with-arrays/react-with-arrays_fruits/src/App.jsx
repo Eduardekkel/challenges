85708a2ca28f667283bda5463ed5ceed0ba3ae36
Eduard Ekkel <eduardekkel.98@gmail.com>
2024-08-28 12:03:49 +0200

    fruits solved, without bonus

--- react-basics/react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-basics/react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,13 +1,45 @@
 import Card from "./components/Card";
 import "./App.css";
 
-
 export default function App() {
-  const fruits = [];
-
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 7034,
+      name: "🍇 Grape",
+      color: "violet",
+    },
+    {
+      id: 4444,
+      name: "🍓 Strawberry",
+      color: "red",
+    },
+    {
+      id: 1222,
+      name: "🥑 Avocado",
+      color: "green",
+    },
+    {
+      id: 2345,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+  ];
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} color={fruit.color} />
+      ))}
     </div>
   );
+
+  // return (
+  //   <div className="app">
+  //     <Card name="🍌 banana" />
+  //   </div>
+  // );
 }
