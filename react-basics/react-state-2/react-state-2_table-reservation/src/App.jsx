29fbd12f9d108c206fe58c0a87ade9e064c7572a
Eduard Ekkel <eduardekkel.98@gmail.com>
2024-08-28 16:54:01 +0200

    table-reservation solved

--- react-basics/react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react-basics/react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 import "./App.css";
 
-export default function App() {
+export default function App({ onClickAdd, OnClickMinus }) {
+  let [people, setPeople] = useState(0);
+  function handleClickAdd({ onClickAdd }) {
+    setPeople((people += 1));
+  }
+  function handleClickMinus(OnClickMinus) {
+    if (people >= 1) setPeople((people -= 1));
+  }
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onClickAdd={handleClickAdd} onClickMinus={handleClickMinus} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
