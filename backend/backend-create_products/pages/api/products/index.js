a96c6b1da0a36f6c83f4cc1fd1baf3417186f6f7
Eduard Ekkel <eduardekkel.98@gmail.com>
2024-09-20 10:26:29 +0200

    chllg solved create

--- backend/backend-create_products/pages/api/products/index.js
+++ backend/backend-create_products/pages/api/products/index.js
@@ -8,4 +8,14 @@ export default async function handler(request, response) {
     const products = await Product.find();
     return response.status(200).json(products);
   }
+  try {
+    request.method === "POST";
+    const productData = request.body;
+    await Product.create(productData);
+
+    return response.status(201).json({ status: "Product created" });
+  } catch (error) {
+    console.log("An error happened:", error);
+    return response.status(400).json({ error: error.message });
+  }
 }
